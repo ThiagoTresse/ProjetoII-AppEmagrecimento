f603f2f09af23d1e43328130b57713b02387b7e3
“labdatabase” <howardcruzroatti@gmail.com>
2023-10-11 00:06:50 -0300

    tabela alimentos e dieta feitos

---
+++ CREATE TABLE dieta (.sql
@@ -0,0 +1,30 @@
+CREATE TABLE dieta (
+    ID INT NOT NULL AUTO_increment,
+    alimento VARCHAR (50) NOT NULL,
+    calorias INT NOT NULL,
+    proteinas INT NOT NULL,
+    carboidratos INT NOT NULL,
+    gordura INT, NOT, NULL,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Arroz integral', 124, 2.6, 25.8, 1); 
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Pao integral', 232, 7.6, 39.9, 3);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Queijo ricota', 140, 12.6, 3.8, 8.1);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Queijo mucarela', 330, 22.6, 3, 25.2);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Laranja inteira', 37, 1, 8.9, 0.1);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Ovo frito', 240, 15.6, 1.2, 18.6);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Ovo cozido', 146, 13.3, 0.6, 9.5);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Batata doce cozida', 77, 0.6, 18.4, 0.1);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Batata cozida', 52, 1.2, 11.9, 0);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Batata frita', 267, 5, 35.6, 13.1);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Pipoca tradicional', 448,  9.9, 70.3, 15.9);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Abacate', 96, 1.2, 6, 8.4);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Banana prata', 98, 1.3, 26,, 0.1);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Maca', 56, 0.3, 15.2, 0);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Tapioca pura', 356, 0.3, 87.5, 0.2);;
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES  ('Iogurte natural desnatado', 41, 3.8, 5.8, 0.3);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Iogurte natural integral', 51, 4.1, 1.9, 3);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Peito de frango grelhado', 159, 32, 0, 2.5);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Carne borvina grelhada', 278, 32.4, 0, 15.5);
+INSERT INTO dieta (alimento, calorias, proteinas, carboidratos, gordura) VALUES ('Tofu', 64, 6.6, 2.1, 4);
